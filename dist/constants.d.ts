f2607798e635a9b4b0b8e5e45b7f3320ec73c49b
jamesbwn <raxioma@outlook.com>
2022-11-09 09:27:09 +1000

    sdk router

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,9 +1,9 @@
-import JSBI from 'jsbi'
-export declare const FACTORY_ADDRESS = '0x3E91CeE56B1A6d16A8d60A269325bDe05C563979'
-export declare const INIT_CODE_HASH = '0xb50cb8b977ceec295d05ff845f69eea4b7b221b185f4bb25e020a6bbce05dc6b'
-export declare const MINIMUM_LIQUIDITY: JSBI
-export declare const ZERO: JSBI
-export declare const ONE: JSBI
-export declare const FIVE: JSBI
-export declare const _997: JSBI
-export declare const _1000: JSBI
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xd592B3a0CB5C262c14250a21dD121451b2187307";
+export declare const INIT_CODE_HASH = "0x9ff3fa50793cbf519adb1e1594682d293c972e4d877f26c72191c7d6ffda1f92";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x3E91CeE56B1A6d16A8d60A269325bDe05C563979' // Tyche Factory on TestNet of EVMOS
+export const FACTORY_ADDRESS = '0xd592B3a0CB5C262c14250a21dD121451b2187307' // Tyche Factory on TestNet of EVMOS
 
-export const INIT_CODE_HASH = '0xb50cb8b977ceec295d05ff845f69eea4b7b221b185f4bb25e020a6bbce05dc6b'
+export const INIT_CODE_HASH = '0x9ff3fa50793cbf519adb1e1594682d293c972e4d877f26c72191c7d6ffda1f92'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
